27c98de766d23fb8d1cce4fc0c40c00eef1fd7e6
McDic <spongbob9876@naver.com>
2019-03-31 21:32:20 +0900

    ~ 2019/03/31 Removed endless import loop

--- connection/__init__.py
+++ connection/__init__.py
@@ -2,7 +2,3 @@
 <module AutoTrade.connection.__init__>
 Provides important namespaces from whole connection submodule.
 """
-
-from connection.errors import *
-from connection.crypto_exchanges import CCXTConnection
-from connection.http.cryptocompare import CryptoCompare
