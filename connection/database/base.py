cdd08b584ab42392a8d27ddf14ca118408f251e8
McDic <spongbob9876@naver.com>
2019-04-12 15:29:38 +0900

    Improved, added file binder, added custom exceptions

--- connection/database/base.py
+++ connection/database/base.py
@@ -7,7 +7,7 @@ Abstract base of all database connection.
 # Libraries
 
 # Standard libraries
-import logging
+import re
 import asyncio
 
 # External libraries
@@ -34,11 +34,11 @@ class AbstractPGDBConnectionClass(AbstractConnection):
 
     def __init__(self, userName: str, password: str, DBname: str,
                  host: str = defaultHost, port: int = defaultPortNumber,
-                 connectionName: str = None, callLimits: dict = None, keys: dict = None):
+                 connectionName: str = None, callLimits: dict = None):
 
         # Parent class initialization
         if connectionName is None: connectionName = "no_named"
-        super().__init__(connectionName = connectionName, callLimits = callLimits, keys = keys)
+        super().__init__(connectionName = connectionName, callLimits = callLimits)
         self.host, self.username, self.port, self.DBname = host, userName, port, DBname
 
         # Async initialization is not completed, you need to it.
@@ -55,7 +55,8 @@ class AbstractPGDBConnectionClass(AbstractConnection):
         self.__initialized_async = True
 
         # Init async PostgreSQL connection
-        self.connection = await asyncpg.connect(host = host, port = port, user = userName, password = password, database = DBname)
+        self.connection = await asyncpg.connect(host = host, port = port, user = userName,
+                                                password = password, database = DBname)
 
         # Exact server version
         exactServerVersion = self.connection.get_server_version()
@@ -74,20 +75,36 @@ class AbstractPGDBConnectionClass(AbstractConnection):
     # Query: Helpers and light queries
 
     @staticmethod
-    def RTN(command: str, *tableNames, tableSign: str = '{T}', varTableSign: str = '{vT}'):
+    def _isCorrectSign(sign: str):
         """
-        <method AbstractPGDBConnectionClass.RTN>
-        RTN = Replace part by Table Name.
+        <method AbstractPGDBConnectionClass._isCorrectSign>
+        :return: If given sign is correct or not.
+        """
+        if sign.startswith('{') and sign.endswith('}'):
+            if "\n" in sign or "$" in sign: return False
+            else: return True
+        return False
+
+    defaultNameSign = "{T}"
+    defaultVarNameSign = "{vT}"
+    @staticmethod
+    def RN(query: str, *names, nameSign: str = defaultNameSign, varNameSign: str = defaultVarNameSign):
+        """
+        <static method AbstractPGDBConnectionClass.RN>
+        RN = Replace part by names
         Generate query by replacing some part of command to table names.
         Use this method when you need to specify table name in query by variable,
             don't use this method when you need to pass table name itself as query variable.
-        :param command: Given command to insert table names.
-        :param tableNames: Table names.
-        :param tableSign: Considered as table names.
+        :param query: Given command to insert table names.
+        :param names: Table names.
+        :param nameSign: Considered query's part as names.
         :return: Generated query.
         """
-        if '%' in tableSign: raise cerr.InvalidValueError("'%' is in tableSign")
-        return command.replace("%", "%%").replace(tableSign, "\"%s\"").replace(varTableSign, "'\"%s\"'") % tableNames
+        if not AbstractPGDBConnectionClass._isCorrectSign(nameSign):
+            raise cerr.InvalidValueError("Given nameSign is not correct(%s)" % (nameSign,))
+        elif not AbstractPGDBConnectionClass._isCorrectSign(varNameSign):
+            raise cerr.InvalidValueError("'%' is in varNameSign")
+        return query.replace("%", "%%").replace(nameSign, "\"%s\"").replace(varNameSign, "'\"%s\"'") % names
 
     async def getColumns(self, tableName: str):
         """
@@ -96,9 +113,9 @@ class AbstractPGDBConnectionClass(AbstractConnection):
         If given table not exists, error will be raised automatically.
         """
         async with self.connection.transaction():
-            columns = [row[0] for row in await self.connection.fetch(
-                "SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = $1", tableName)]
-        return columns
+            columnNames = await self.connection.fetch(
+                "SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = $1", tableName)
+        return [row[0] for row in columnNames]
 
     async def getPrimaryKeys(self, tableName: str):
         """
@@ -107,23 +124,43 @@ class AbstractPGDBConnectionClass(AbstractConnection):
         If given table not exists, error will be raised automatically.
         """
         async with self.connection.transaction():
-            primaryKeys = [row[0] for row in await self.connection.fetch(self.RTN("""
+            primaryKeys = await self.connection.fetch(self.RN("""
                 SELECT a.attname AS key_name
                 FROM   pg_index i
                 JOIN   pg_attribute a ON a.attrelid = i.indrelid AND a.attnum = ANY(i.indkey)
                 WHERE  i.indrelid = {vT}::regclass AND i.indisprimary;
-            """, tableName))]
-        print("Fetched primary keys: %s" % (primaryKeys,))
-        return primaryKeys
+            """, tableName))
+        return [row[0] for row in primaryKeys]
 
     # ------------------------------------------------------------------------------------------------------------------
     # Query: Generic and heavy queries
+
+    async def execute(self, query: str, tableNames: tuple = (), *args,
+                      toleratedExceptions: tuple = (), timeout: float = None,
+                      tableSign: str = defaultNameSign, varTableSign: str = defaultVarNameSign):
+        """
+        <async method AbstractPGDBConnectionClass.execute>
+        :param query: Base query to execute.
+        :param tableNames, tableSign, varTableSign: Table names and table signs to replace.
+        :param args: Argument values passed into 'self.connection.execute'.
+        :param toleratedExceptions: List of tolerated exceptions.
+            If one of the exception in toleratedExceptions raised, it's tolerated.
+        :param timeout: Timeout used in 'self.connection.execute'.
+        :return: The given query was successful or not.
+            This doesn't implies that your query was executed in your intend.
+        """
+        query = self.RN(query, *tableNames, nameSign= tableSign, varNameSign= varTableSign)
+        print("Will try query: %s" % (query,))
+        async with self.connection.transaction(): # Transaction or savepoint begin
+            try: await self.connection.execute(query, *args, timeout = timeout)
+            except toleratedExceptions: return False
+            else: return True
 
     async def pushFile(self, fileName, tableName, columns: tuple = None, timeout: float = None,
                        delimiter = ",", override: bool = True):
         """
         <async method AbstractPGDBConnectionClass.pushFile>
-        Copy data from file to given table.
+        Copy data from file to given table with resolving conflicts.
         Precise of file data syntax: (Note that ',' is base delimiter, other delimiter can be used.)
             a11, a12, a13, ..., a1c
             a21, a22, a23, ..., a2c
@@ -133,35 +170,31 @@ class AbstractPGDBConnectionClass(AbstractConnection):
             Each row of content of file should be ordered by given columns order or default order.
         :param tableName: The name of target table.
         :param columns: Provide order of columns of each row of content of given file.
+            Precise syntax: None | (c[1], c[2]..., c[c]) where c[i] is name of i-th column in data file.
         :param timeout: Query timeout deadline. None for non-deadline.
         :param override: Decide to override conflicted data or not.
         """
 
-        # Step 1: Find all columns and primary keys and validate given column order(if provided) is valid.
-        tableColumns = await self.getColumns(tableName)
-        primaryKeys = await self.getPrimaryKeys(tableName)
-        if columns: # If column order given, all column name should be distinct and all primary key should be happened.
-            pass
-
-        # Step 2: Copy data to empty temporary table,
+        # Copy data to empty temporary table,
         async with self.connection.transaction():
 
-            # Step 2-1: Create temporary table with very rare name, it's dropped after commit.
+            # Step 1: Create temporary table with very rare name, it's dropped after commit.
             tempTableName = "_temp_table_name_that_used_in_AbstractPDGBConnection_"
-            await self.connection.execute(
-                # self.RTN("DROP TABLE IF EXISTS {T}", tempTableName),
-                self.RTN("CREATE TEMPORARY TABLE {T} (LIKE {T}) ON COMMIT DROP", tempTableName, tableName),
-                timeout = timeout
-            )
+            await self.execute("CREATE TEMPORARY TABLE {T} (LIKE {T}) ON COMMIT DROP",
+                               (tempTableName, tableName), timeout = timeout)
 
-            # Step 2-2: Copy file data to temp table.
-            await self.connection.copy_to_table(tempTableName, columns = columns,
-                source = open(fileName, "rb"), delimiter = delimiter, timeout = timeout)
+            # Step 2: Copy file data to temp table.
+            with open(fileName, "rb") as sourceFile:
+                await self.connection.copy_to_table(tempTableName, columns = columns,
+                    source = sourceFile, delimiter = delimiter, timeout = timeout)
 
-            # Step 2-3: Copy rows from temp table to main table, and drop temp table again.
+            # Step 3: Generate insert query suffix by given columns and calculating primary keys and total columns.
             if override:
+                primaryKeys = set(await self.getPrimaryKeys(tableName)) # Get primary keys
                 if columns: updateColumns = tuple(columns)
-                else: updateColumns = [column for column in tableColumns if column not in primaryKeys]
+                else:
+                    tableColumns = await self.getColumns(tableName) # Get all column names
+                    updateColumns = [column for column in tableColumns if column not in primaryKeys]
                 if len(updateColumns) > 1:
                     updateQuerySuffix = "(" + ", ".join("\"" + primaryKey + "\"" for primaryKey in primaryKeys) \
                         + ") DO UPDATE SET (" + ", ".join("\"" + column + "\"" for column in updateColumns) \
@@ -170,19 +203,65 @@ class AbstractPGDBConnectionClass(AbstractConnection):
                     updateQuerySuffix = "(" + ", ".join("\"" + primaryKey + "\"" for primaryKey in primaryKeys) \
                         + ") DO UPDATE SET \"" + updateColumns[0] + "\" = EXCLUDED.\"" + updateColumns[0] + "\""
             else: updateQuerySuffix = "DO NOTHING"
-            lastQuery = self.RTN("INSERT INTO {T} (SELECT * FROM {T}) ON CONFLICT " + updateQuerySuffix, tableName, tempTableName)
-            await self.connection.execute(lastQuery, timeout = timeout)
 
-    # ------------------------------------------------------------------------------------------------------------------
+            # Step 4: Copy rows from temp table to main table, and drop temp table again.
+            lastQuery = "INSERT INTO {T} (SELECT * FROM {T}) ON CONFLICT " + updateQuerySuffix
+            await self.execute(lastQuery, (tableName, tempTableName), timeout = timeout)
 
-async def AbstractPGDBConnection(userName: str, password: str, DBname: str,
+    tableGrantPrivileges = ["SELECT", "INSERT", "UPDATE", "DELETE", "TRUNCATE", "REFERENCES", "TRIGGER", "ALL"]
+    async def grantTable(self, privilege: str, userName: str = None, tableName: str = None, schema: str = "public",
+                         withGrantOption: bool = False, grant: bool = True):
+        """
+        <async method AbstractPGDBConnectionClass.grantTable>
+        Execute grantTable query with given options.
+        :param privilege: Privilege to grant or revoke. None or empty string implies "ALL".
+        :param userName: User name to grant or revoke. If None given then give to "PUBLIC".
+        :param tableName: Table name to specify. If None given then grantTable/revoke on all tables in given schema.
+        :param schema: Schema name to specify. Default to "public".
+        :param withGrantOption: Determine to give grant option or not.
+        :param grant: Determine to grant(True) or revoke(False).
+        """
+        if not privilege: privilege = "ALL"
+        if privilege not in AbstractPGDBConnectionClass.tableGrantPrivileges:
+            raise InvalidQueryError("Given privilege(%s) is not valid" % (privilege,))
+        query = ("GRANT" if grant else "REVOKE") + " " + privilege + " ON " + \
+                (self.RN("TABLE {T}", tableName) if tableName else self.RN("ALL TABLES IN SCHEMA {T}", schema)) + \
+                " " + ("TO" if grant else "FROM") + " " + (self.RN("{T}", userName) if userName else "PUBLIC") + \
+                (" WITH GRANT OPTION" if withGrantOption else "")
+        await self.execute(query)
+
+    async def grantDatabase(self, ): pass
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Exceptions
+
+class AbstractPGDBError(cerr.AutoTradeConnectionError, asyncpg.exceptions.PostgresError):
+    """
+    <class AbstractPGDBError> inherited from AutoTradeConnectionError, asyncpg.exceptions.PostgresError
+    Abstract base of all exceptions for AbstractPGDBConnectionClass.
+    """
+
+class InvalidQueryError(AbstractPGDBError, asyncpg.exceptions.QueryCanceledError):
+    """
+    <class InvalidQueryError> inherited from AbstractPGDBError, asyncpg.exceptions.QueryCanceledError
+    Raised when the query is invalid.
+    """
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Asynchronized init binder
+async def AbstractPGDBConnection(userName: str = None, password: str = None, DBname: str = None,
                                  host: str = AbstractPGDBConnectionClass.defaultHost,
-                                 port: int = AbstractPGDBConnectionClass.defaultPortNumber, **kwargs):
+                                 port: int = AbstractPGDBConnectionClass.defaultPortNumber,
+                                 fileName: str = None, **kwargs):
     """
     <function AbstractPGDBConnection>
-    Construct and return Abstract PostgreSQL Connection.
+    Construct and return Abstract PostgreSQL Connection asynchronously.
     I recommend you to use this function to get Connection object instead of direct instantiation.
     """
+    if userName is None and password is None and DBname is None: # Should recover information from file
+        if fileName is None: raise cerr.InvalidError("None of mandatory arguments were given.")
+        with open(fileName, "r") as authFile:
+            userName, password, host, port, DBname = [c.strip(' ') for c in authFile.read().split("\n")]
     connection = AbstractPGDBConnectionClass(userName = userName, password = password, DBname = DBname,
                                              host = host, port = port, **kwargs)
     await connection._init_async(host, port, userName, password, DBname)
@@ -194,17 +273,17 @@ async def AbstractPGDBConnection(userName: str, password: str, DBname: str,
 if __name__ == "__main__":
 
     from pprint import pprint
+    import random
     print(AbstractPGDBConnectionClass.__doc__)
 
     async def run():
-        PGDB = await AbstractPGDBConnection("McDicBot", "1234", "postgres", connectionName ="TestConnection")
-        result1 = await PGDB.connection.execute(PGDB.RTN("TRUNCATE {T}", "TEST3"))
-        await PGDB.connection.execute("INSERT INTO \"TEST3\" VALUES ($1, $2) ON CONFLICT DO NOTHING", 103578, -7.2)
-        result3 = await PGDB.connection.fetch(PGDB.RTN("SELECT * FROM {T}", "TEST3"))
-        pprint(result1)
-        pprint(result3)
-        await PGDB.pushFile("../../test/data/test_insert.txt", "TEST3", override = True)
-        result_again = await PGDB.connection.fetch(PGDB.RTN("SELECT * FROM {T}", "TEST3"))
-        pprint(result_again)
 
-    asyncio.get_event_loop().run_until_complete(run())
+        PGDB = await AbstractPGDBConnection(fileName = "awsdb.authkey")
+        await PGDB.grantTable("ALL", userName = "m43ng")
+
+    async def run_m43ng():
+
+        PGDB = await AbstractPGDBConnection(fileName = "awsdb_m43ng.authkey")
+        pprint(await PGDB.connection.fetch("SELECT * FROM \"PriceData_Bitfinex_USD_BTC_1mins\" LIMIT 100"))
+
+    asyncio.get_event_loop().run_until_complete(run_m43ng())
