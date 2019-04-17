c7fe347894798b1fb6cc6715b8090a8d0721ffc2
m43ng <shinmg2520@gmail.com>
2019-04-17 19:55:42 +0900

    requests_future -> aiohttp

--- connection/http/base.py
+++ connection/http/base.py
@@ -7,6 +7,8 @@ This module is used to describe the abstraction of all type of HTTP connection.
 
 # Standard libraries
 import re
+import atexit
+import copy
 
 # External libraries
 import aiohttp
@@ -14,50 +16,50 @@ import asyncio
 
 # Custom libraries
 import connection.base, connection.errors
+from connection.http.errors import *
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Abstract base
 
-class AbstractHTTPConnection(connection.base.AbstractConnection):
+class AbstractHTTPConnectionClass(connection.base.AbstractConnection):
     """
     <class AbstractHTTPConnection>
     Abstract base of all HTTP connections.
     """
 
-    # ----------------------------------------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------------------------------------
     # HTTP exceptions codes
     
     httpExceptions = {
-        422 : 'ServiceError',
-        418 : 'DDoSProtection',
-        429 : 'DDoSProtection',
-        404 : 'ServiceNotAvailable',
-        409 : 'ServiceNotAvailable',
-        500 : 'ServiceNotAvailable',
-        501 : 'ServiceNotAvailable',
-        502 : 'ServiceNotAvailable',
-        520 : 'ServiceNotAvailable',
-        521 : 'ServiceNotAvailable',
-        522 : 'ServiceNotAvailable',
-        525 : 'ServiceNotAvailable',
-        526 : 'ServiceNotAvailable',
-        400 : 'ServiceNotAvailable',
-        403 : 'ServiceNotAvailable',
-        405 : 'ServiceNotAvailable',
-        503 : 'ServiceNotAvailable',
-        530 : 'ServiceNotAvailable',
-        408 : 'RequestTimeout',
-        504 : 'RequestTimeout',
-        401 : 'AuthenticationError',
-        511 : 'AuthenticationError',
+        422 : ServiceError,
+        418 : DDoSProtection,
+        429 : DDoSProtection,
+        404 : ServiceNotAvailable,
+        409 : ServiceNotAvailable,
+        500 : ServiceNotAvailable,
+        501 : ServiceNotAvailable,
+        502 : ServiceNotAvailable,
+        520 : ServiceNotAvailable,
+        521 : ServiceNotAvailable,
+        522 : ServiceNotAvailable,
+        525 : ServiceNotAvailable,
+        526 : ServiceNotAvailable,
+        400 : ServiceNotAvailable,
+        403 : ServiceNotAvailable,
+        405 : ServiceNotAvailable,
+        503 : ServiceNotAvailable,
+        530 : ServiceNotAvailable,
+        408 : RequestTimeout,
+        504 : RequestTimeout,
+        401 : AuthenticationError,
+        511 : AuthenticationError,
     }
 
-    # ----------------------------------------------------------------------------------------------------------------------
-    # Base methods
+    # ------------------------------------------------------------------------------------------------------------------
+    # Initializing methods
 
     def __init__(self, baseURL : str, connectionName: str = "AbstractHTTPConnection",
-                 callLimits = None, keys = None, timeout : int = None, headers = None,
-                 cookies = None, loop = None, hook = None):
+                 callLimits = None, keys = None, timeout : int = None, hooks = None):
         """
         <method AbstractHTTPConnection.__init__>
         :param connectionName:  The name of this connection.
@@ -79,12 +81,34 @@ class AbstractHTTPConnection(connection.base.AbstractConnection):
             timeout = 1 * 60    # 1 minutes
         self.timeout = aiohttp.ClientTimeout(total=timeout)
 
-        # Setting Loop
-        if loop is None:
-            self.loop = asyncio.get_event_loop()
+
+        # Initialize session to none
+        self.session = None
+
+        # hook check
+        if hooks is None:
+            self.hooks = []
+        elif hasattr(hooks,'__call__'):
+            self.hooks = [hooks]
         else:
-            self.loop = loop
-        
+            try:
+                self.checkValidHookList(hooks)
+            except (connection.errors.InvalidTypeError, connection.errors.InvalidError):
+                raise
+            self.hooks = hooks
+
+        # Register terminate function at exit
+        atexit.register(self.terminate)
+
+    async def _init_async(self, headers = None, cookies = None):
+        """
+        <async method AbstractHTTPConnection._init_async>
+        Async initializing function
+        :param headers: parameter for ClientSession
+        :param cookies: parameter for ClientSession
+        :return:
+        """
+
         # Initialize header if None
         headers = headers or {}
         # Evading crawling prevention
@@ -93,14 +117,8 @@ class AbstractHTTPConnection(connection.base.AbstractConnection):
         # Quicker network response
         headers.update({'Accept-Encoding': 'gzip, deflate'})
 
-        # Initialize session
-        self.session = None
-        async def initSession(headers = headers, cookies = cookies):
-            if self.session is None:
-                self.session = aiohttp.ClientSession(headers = headers, cookies = cookies, timeout = self.timeout)
-        self.loop.run_until_complete(initSession(headers = headers, cookies = cookies))\
-
-
+        if self.session is None:
+            self.session = aiohttp.ClientSession(headers = headers, cookies = cookies, timeout = self.timeout)
 
     def __str__(self): return "Abstract HTTP Connection [%s]" % (self.name,)
     
@@ -108,13 +126,48 @@ class AbstractHTTPConnection(connection.base.AbstractConnection):
     # Termination
 
     def terminate(self):
-        async def closeSession():
-            if self.session is not None:
-                await self.session.close()
-        self.loop.run_until_complete(closeSession())
-        self.session = None            
+        """
+        <method AbstractHTTPConnection.terminate>
+        sync function to terminate class
+        :return:
+        """
+        asyncio.get_event_loop().run_until_complete(self._terminate_async())
 
-    # ----------------------------------------------------------------------------------------------------------------------
+    async def _terminate_async(self):
+        """
+        <async method AbstractHTTPConnection._terminate_async>
+        async function to terminate class
+        :return:
+        """
+        await self.session.close()
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # Hooking part
+    @staticmethod
+    def checkValidHookList(hooks):
+        """
+        <static method AbstractHTTPConnection.checkValidHookList>
+        Check whether given hook list is valid or not
+        :param hooks:    given hook list
+        :return:
+        """
+        if not isinstance(hooks, list):
+            raise connection.errors.InvalidTypeError('hooks must be "list" of callable functions')
+        else:
+            if all([hasattr(h, '__call__') for h in hooks]):
+                pass
+            else:
+                raise connection.errors.InvalidError('hooks must be list of "callable" functions')
+
+    def concatenateHooks(self, funcHooks: list):
+        """
+        hooks concatenating function
+        :param funcHooks:    function's individual hooks
+        :return:
+        """
+        return copy.deepcopy(self.hooks) + funcHooks
+
+    # ------------------------------------------------------------------------------------------------------------------
     # Class methods
 
     def targetURL(self, endpoint: str):
@@ -127,7 +180,7 @@ class AbstractHTTPConnection(connection.base.AbstractConnection):
         return self.baseURL + "/" + endpoint
 
     async def request(self, mode: str, endpoint: str,
-                params = None, data = None, json = None):
+                params = None, data = None, json = None, hooks = None):
         """
         <method AbstractHTTPConnection.request>
         Create requesting coroutine.
@@ -139,14 +192,12 @@ class AbstractHTTPConnection(connection.base.AbstractConnection):
         :return Request coroutine, after executing coroutine, result can be acquired by <return value>.text() etc.
         """
 
-
         # Data, Json arguments can not be passed at the same time
         if data is not None and json is not None:
             raise connection.errors.InvalidError("Data, Json arguments can not be passed at the same time")
 
         # Make asynchronous request coroutine
         targetURL = self.targetURL(endpoint)
-        print(targetURL)
         result = None
         mode = mode.upper()
         if mode == "POST":      result = await self.session.post(targetURL, data = data, json = json)
@@ -154,28 +205,64 @@ class AbstractHTTPConnection(connection.base.AbstractConnection):
         elif mode == "PUT":     result = await self.session.put(targetURL, data = data, json = json)
         elif mode == "DELETE":  result = await self.session.delete(targetURL)
         else:                   raise connection.errors.InvalidError("Invalid mode(%s) to request" % (mode,))
-        print(await result.json(), result.status)
+
+        # Hooking part
+        if hooks is None:
+            hooks = []
+        elif hasattr(hooks, '__call__'):
+            hooks = [hooks]
+        else:
+            try:
+                self.checkValidHookList(hooks)
+            except (HTTPConnectionError):
+                raise
+            hooks = hooks
+        finalHook = self.concatenateHooks(hooks)
+        
+
+
         # Check http status exceptions
         error = None
         if result.status in self.httpExceptions:
             error = self.httpExceptions[result.status]
-            if error == 'ServiceNotAvailable':
+            if isinstance(error,ServiceNotAvailable):
                 if re.search('(cloudflare|incapsula|overload|ddos)', await result.text(), flags=re.IGNORECASE):
-                    error = 'DDoSProtection'
+                    error = DDoSProtection
         if error:
-            raise connection.errors.InvalidError("HTTP status exception : HTTP code %s error(%s)" % (result.status, error))
+            raise error
         
         return result
 
+# ----------------------------------------------------------------------------------------------------------------------
+# Binder of AbstractHTTPConnectionClass
+
+async def AbstractHTTPConnection(baseURL : str, connectionName: str = "AbstractHTTPConnection",
+                 callLimits = None, timeout : int = None, headers = None,
+                 cookies = None, hooks = None):
+    connection = AbstractHTTPConnectionClass(baseURL, connectionName = connectionName, callLimits = callLimits,
+                                             timeout = timeout, hooks = hooks)
+    await connection._init_async(headers = headers, cookies = cookies)
+    return connection
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Testing
 
+def printA():
+    print("A")
+
+def printB():
+    print("B")
+
+def printC():
+    print("C")
 
 if __name__ == "__main__":
+    async def run():
+        hooks = [printA, printB]
+        abc = await AbstractHTTPConnection('http://gilgil.net/asfefe',hooks = hooks)
+        co = await abc.request('get','', hooks = [printC])
+        print (await co.text())
+
     loop = asyncio.get_event_loop()
-    ahc = AbstractHTTPConnection('https://api.kraken.com')
-    temp = ahc.request('get', '0/public/Assets', '', 0)
-    result = loop.run_until_complete(temp)
-    result_txt = loop.run_until_complete(result.json())
-    print(result_txt)
+    loop.run_until_complete(run())
+
--- connection/http/cryptocompare.py
+++ connection/http/cryptocompare.py
@@ -6,49 +6,48 @@ HTTP connection for cryptocompare.com to get historical data.
 # Libraries
 
 # Standard libraries
+import asyncio
 import time
+from pprint import pprint
 
 # External libraries
 
 # Custom libraries
-from connection.http.base import AbstractHTTPConnection
+from connection.http.base import AbstractHTTPConnectionClass, AbstractHTTPConnection
 
 # ----------------------------------------------------------------------------------------------------------------------
 # CryptoCompare
 
-class CryptoCompare(AbstractHTTPConnection):
+class CryptoCompareClass(AbstractHTTPConnectionClass):
     """
     <class CryptoCompare>
     Used to interact between client and CryptoCompare.com to get historical price data.
     """
 
-    def __init__(self, api_key):
+    def __init__(self):
         """
         <method CryptoCompare.__init__>
         :param api_key: API key for CryptoCompare.
         """
 
         # Parent class initialization
-        super().__init__(connectionName = "CryptoCompare data loader", keys= {"api": api_key},
-                         baseURL = "https://min-api.cryptocompare.com",
-                         callLimits = {"default": (60*60*24*31, 10**5)})
+        super().__init__(connectionName = "CryptoCompare Abstract Connection",
+                         baseURL = "https://min-api.cryptocompare.com")
 
-        # API key register on session header
-        self.session.headers["authorization"] = "Apikey " + self.key["api"]
 
-    def rateLimit(self):
+    def getRateLimit(self):
         """
-        <method CryptoCompare.rateLimit>
-        Used to get current rate limit.
+        <method CryptoCompare.getRateLimit>
+        Used to get current rate limit information
         ex) https://min-api.cryptocompare.com/stats/rate/limit
-        :return: Request object
+        :return: Request object coroutine
         """
 
         # Create request and return
-        req = self.request("GET", "stats/rate/limit", "default", 1)
+        req = self.request("GET", "stats/rate/limit")
         return req
 
-    def historicalMinuteOHLCV(self, baseCurrency, targetCurrency,
+    def historicalMinuteOHLCV(self, baseCurrency: str, targetCurrency: str,
                               startTimestamp: int, endTimestamp: int,
                               exchange = "CCCAGG"):
         """
@@ -58,29 +57,59 @@ class CryptoCompare(AbstractHTTPConnection):
         :param baseCurrency:    Base currency to search. ex) USD, KRW, BTC, USDT
         :param targetCurrency:  Quote currency to search. ex) BTC, ETH, XRP
         :param startTimestamp:  Start timestamp.
-        :param endTimestamp:    End timestamp. Search [startTimestamp, endTimestamp]
+        :param endTimestamp:    End timestamp. Search [startTimestamp, endTimestamp] but no more than 2000 data
         :param exchange:        Exchange. CCCAGG is the default value of CryptoCompare.com
         :return: Request object
         """
 
         # Round timestamps
-        assert isinstance(startTimestamp, int) and isinstance(endTimestamp, int)
+        if not isinstance(startTimestamp, int) or not isinstance(endTimestamp, int):
+            raise TypeError("Both timestamps must be type of int")
+
         if startTimestamp % 60: startTimestamp += 60 - startTimestamp % 60
         endTimestamp -= endTimestamp % 60
 
-        # Create request and return
-        req = self.request("GET", "data/histominute", "default", 1,
+        # Check limit info
+        if (endTimestamp - startTimestamp)/60 > 2000:
+            raise ValueError("You can't fetch data more the 2000")
+        limit = (endTimestamp - startTimestamp) // 60 + 1
+
+
+        # Create request coroutine and return
+        req = self.request("GET", "data/histominute",
                            params = {"tryConversion": "false", "fsym": baseCurrency, "tsym": targetCurrency,
-                                     "limit": (endTimestamp - startTimestamp) // 60 + 1,
+                                     "limit": limit,
                                      "e": exchange, "toTs": endTimestamp})
         return req
 
+async def CryptoCompare(api_key: str):
+    """
+    <cryptocompare.CryptoCompare>
+    Async binder of CryptoCompareClass
+    :param api_key: api key for cryptocompare authorization
+    :return: CryptoCompareClass initialized
+    """
+    header = {}
+    header.update({'authorization':'Apikey{'+api_key+'}'})
+    connection = CryptoCompareClass()
+    await AbstractHTTPConnectionClass._init_async(connection,headers=header)
+    return connection
 
 if __name__ == "__main__":
-    with open("cryptocompare.key") as cckeyFile: key = cckeyFile.read()
-    cc = CryptoCompare(key)
-    # req = cc.historicalMinuteOHLCV("BTC", "ETH", round(time.time() - 60*60), round(time.time()))
-    req = cc.rateLimit()
-    resp = req.result()
-    print(resp.url)
-    print(resp.json())
+    async def main():
+        with open("cryptocompare.key") as cckeyFile: key = cckeyFile.read()
+        connect = await CryptoCompare(key)
+        # req = await connect.getRateLimit()
+        # print(await req.json())
+        req = await connect.historicalMinuteOHLCV('BTC','USD',int(time.time())-60*10,int(time.time()))
+        print(int(time.time()))
+        pprint(await req.json())
+
+
+        # req = cc.historicalMinuteOHLCV("BTC", "ETH", round(time.time() - 60*60), round(time.time()))
+        # req = cc.rateLimit()
+        # resp = req.result()
+        # print(resp.url)
+        # print(resp.json())
+
+    asyncio.get_event_loop().run_until_complete(main())
